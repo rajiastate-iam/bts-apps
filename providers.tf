225b5e0b76a66ed81fcf2c685bc05ae3aac66359
rajiastate <raj11.iastate@gmail.com>
2026-01-03 16:51:16 -0600

    added local state

--- providers.tf
+++ providers.tf
@@ -7,6 +7,18 @@ terraform {
       version = "~> 1.37.0"
     }
   }
+
+  # POC: local backend, state stored in repo as terraform.tfstate
+  # TODO: Replace this with an Azure Blob Storage backend for production:
+  # backend "azurerm" {
+  #   resource_group_name  = "RG-NAME"
+  #   storage_account_name = "STORAGEACCOUNTNAME"
+  #   container_name       = "tfstate"
+  #   key                  = "auth0-iam/terraform.tfstate"
+  # }
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
 
 provider "auth0" {
